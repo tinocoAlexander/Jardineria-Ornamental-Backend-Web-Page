948361f74d0c4967dcd4994e92927501c4f4aeea
Alexander Tinoco Sanchez <alexnuevo040@gmail.com>
2025-06-25 18:32:04 -0600

    fix: restructure all the backend

--- prisma/migrations/20250605012717_status_for_appointment/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Appointment" ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'PENDING';
--- services/src/types/service.types.ts
+++
@@ -1,4 +0,0 @@
-export interface Service {
-    name: string;
-    description?: string;
-}
